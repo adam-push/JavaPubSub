fbaa0c49604ed18c39d49dd36afce0ca42d52ed8
Adam Turnbull <adam.turnbull@diffusiondata.com>
2025-01-30 20:13:03 +0000

    Use of UpdateStream

--- src/main/java/com/diffusion/training/Publisher.java
+++ src/main/java/com/diffusion/training/Publisher.java
@@ -2,7 +2,7 @@ package com.diffusion.training;
 
 import com.pushtechnology.diffusion.client.Diffusion;
 import com.pushtechnology.diffusion.client.features.TopicUpdate;
-import com.pushtechnology.diffusion.client.features.control.topics.TopicControl;
+import com.pushtechnology.diffusion.client.features.UpdateStream;
 import com.pushtechnology.diffusion.client.session.Session;
 import com.pushtechnology.diffusion.client.topics.details.TopicSpecification;
 import com.pushtechnology.diffusion.client.topics.details.TopicType;
@@ -20,10 +20,13 @@ public class  Publisher {
 
         TopicSpecification spec = Diffusion.newTopicSpecification(TopicType.JSON);
 
+        UpdateStream<JSON> stream = session.feature(TopicUpdate.class).newUpdateStreamBuilder()
+                .specification(spec)
+                .build("my/json/topic", JSON.class);
+
         // Update our topic
         for(int i = 0; i < 100; i++) {
-            session.feature(TopicUpdate.class)
-                    .addAndSet("my/json/topic", spec, JSON.class, Diffusion.dataTypes().json().fromJsonString("{\"counter\": " + i +"}"));
+            stream.set(Diffusion.dataTypes().json().fromJsonString("{\"counter\": " + i +"}"));
             Thread.sleep(1000);
         }
 
