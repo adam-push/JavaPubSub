0f898f382200bb6b6dabb9254fe1b26f5630c256
Adam Turnbull <adam.turnbull@diffusiondata.com>
2025-01-30 10:39:52 +0000

    Skeleton subscriber with connect only

--- src/main/java/com/diffusion/training/Subscriber.java
+++ src/main/java/com/diffusion/training/Subscriber.java
@@ -1,8 +1,18 @@
 package com.diffusion.training;
 
+import com.pushtechnology.diffusion.client.Diffusion;
+import com.pushtechnology.diffusion.client.session.Session;
+
 public class Subscriber {
 
     public static void main(String[] args) throws Exception {
+        final Session session = Diffusion.sessions()
+                .open("ws://localhost:8080");
+
+        System.out.println("SessionID = " + session.getSessionId());
+
+        Thread.sleep(1000);
+        session.close();
     }
 
 }
