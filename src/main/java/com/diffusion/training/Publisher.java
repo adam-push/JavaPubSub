b4db419b0b9681a7b95e26de5344dc7909659c93
Adam Turnbull <adam.turnbull@diffusiondata.com>
2025-01-30 10:14:10 +0000

    Publisher connects to Diffusion

--- src/main/java/com/diffusion/training/Publisher.java
+++ src/main/java/com/diffusion/training/Publisher.java
@@ -1,8 +1,19 @@
 package com.diffusion.training;
 
-public class Publisher {
+import com.pushtechnology.diffusion.client.Diffusion;
+import com.pushtechnology.diffusion.client.session.Session;
+
+public class  Publisher {
 
     public static void main(String[] args) throws Exception {
+        final Session session = Diffusion.sessions()
+                .principal("admin")
+                .password("password")
+                .open("ws://localhost:8090");
+
+        System.out.println("SessionID = " + session.getSessionId());
+
+        session.close();
     }
 
 }
