c6e3527b730ffb2ba83ce2208b211424a614d3ad
Adam Turnbull <adam.turnbull@diffusiondata.com>
2025-01-30 09:41:31 +0000

    Add exception to subscriber main

--- src/main/java/com/diffusion/training/Subscriber.java
+++ src/main/java/com/diffusion/training/Subscriber.java
@@ -2,7 +2,7 @@ package com.diffusion.training;
 
 public class Subscriber {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
     }
 
 }
